a74f53b64af16eb0f2ba809cffb9a59afaf6b83c
Jason Kridner <jkridner@beagleboard.org>
2025-11-17 11:55:18 -0500

    Revert "no hang"
    This reverts commit c52ec53a2404b9585803cbb3d22e3d162b4a05d1.

--- src/main.c
+++ src/main.c
@@ -67,7 +67,11 @@ int main(void)
 
 	printk("Reached main()\n");
 	read_an_mb1();
+	printk("Stop network device\n");
 	netdeviceapi->stop(netdevice);
+	read_an_mb1();
+	printk("Start network device\n");
+	netdeviceapi->start(netdevice);
 	read_an_mb1();
 	printk("Exiting main()\n");
 	return(0);
